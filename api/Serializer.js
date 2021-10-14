448e15b930b589e9f8baacbc885f7137eb4adc32
Dandeiz <daniloflorenzano1@outlook.com>
2021-10-14 11:56:08 -0300

    filtering returned data

--- api/Serializer.js
+++ api/Serializer.js
@@ -7,10 +7,36 @@ class Serializer {
 
     serilize(data) {
         if(this.contentType === 'application/json') {
-            return this.json(data);
+            return this.json(
+                this.filter(data)
+            );
         }
 
         throw new NotAllowedValue(this.contentType);
+    }
+
+    objectFilter(data) {
+        const newObject = {};
+
+        this.publicFields.forEach((field) => {
+            if(data.hasOwnProperty(field)) {
+                newObject[field] = data[field];
+            }
+        })
+
+        return newObject;
+    }
+
+    filter(data) {
+        if(Array.isArray(data)) {
+            data = data.map(item => {
+                return this.objectFilter(item);
+            });
+        } else {
+            data = this.objectFilter(data)
+        }
+
+        return data;
     }
 }
 
@@ -18,6 +44,7 @@ class SerializerFornecedor extends Serializer {
     constructor(contentType) {
         super();
         this.contentType = contentType;
+        this.publicFields = ['id', 'empresa', 'categoria']
     }
 }
 
